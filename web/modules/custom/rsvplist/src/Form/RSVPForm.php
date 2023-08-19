f0512c087ae8222a85a6b222b61c121c26b932d7
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-08-19 12:53:39 -0500

    inserting into the database programatically

--- web/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -74,7 +74,65 @@ class RSVPForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // get the value from the 'email' form and place it in a variable
-    $submitted_email = $form_state->getValue('email');
-    $this->messenger()->addMessage(t("The form is working! You entered @entry.", ['@entry' => $submitted_email]));
+    // $submitted_email = $form_state->getValue('email');
+    // $this->messenger()->addMessage(t("The form is working! You entered @entry.", ['@entry' => $submitted_email]));
+    try {
+      // Get current user ID. Not a full user object
+      $uid = \Drupal::currentUser()->id();
+
+      // Demonstration for how to load a full user object of the current user.
+      // This $full_user variable is not needed for this code,
+      // but is shown for demonstration purposes.
+      // Often you need to work with the values of the fields set on the user Entity (name, created date, etc. )
+      $full_user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+
+      // Obtain values as entered into the From
+      $nid = $form_state->getValue('nid');
+      $email = $form_state->getValue('email');
+
+      $current_time = \Drupal::time()->getRequestTime();
+      // End Phase 1
+
+      // Begin Phase 2
+      // Start to build a query builder object $query.
+      // https://www.drupal.org/docs/8/api/database-api/insert-queries
+      $query = \Drupal::database()->insert('rsvplist');
+
+      // Specify the fields that the query will insert into.
+      $query->fields([
+        'uid',
+        'nid',
+        'mail',
+        'created',
+      ]);
+
+      // Set the values of the fields we selected.
+      // Note that they must be in the same order as we defined them
+      // in the $query->fields([...]) above.
+      $query->values([
+        $uid,
+        $nid,
+        $email,
+        $current_time,
+      ]);
+
+      // Execute the query!
+      // Drupal handles the exact syntax of the query automatically!
+      $query->execute();
+      // End Phase 2
+
+      // Begin Phase 3: Display a success message
+
+      // Provide the form submitter a nice message.
+      \Drupal::messenger()->addMessage(
+        t('Thank you for your RSVP, you are on the list for the event!')
+      );
+      // End Phase 3
+    } catch (\Exception $error) {
+      \Drupal::messenger()->addError(
+        t('Unable to save RSVP settings at this time due to database error.
+         Please try again.')
+      );
+    }
   }
 }
