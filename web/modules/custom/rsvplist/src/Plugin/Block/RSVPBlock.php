ddb5a452bb91e62e12a9b06b9e4d63d89328010c
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-08-20 19:39:41 -0500

    displaying rsvp form conditionally

--- web/modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
+++ web/modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
@@ -38,25 +38,16 @@ class RSVPBlock extends BlockBase {
     $node = \Drupal::routeMatch()->getParameter('node');
     // perform an access check to determine if the current user has correct permission
     if (!(is_null($node))) {
-      // allowedIfHasPermission takes two parameters: account to check, and the machine name of the permission to check for
-      return AccessResult::allowedIfHasPermission($account, 'view rsvplist');
+      $enabler = \Drupal::service('rsvplist.enabler');
+      // Some pages may not be nodes, although we could not display the block using the Block Settings through
+      // the Block UI at /admin/structure/block, instead we are programmatically controlling to only display
+      // this block on node pages using AccessResult::allowedIfHasPermission($account, 'view rsvplist')
+      if ($enabler->isEnabled($node)) {
+        // allowedIfHasPermission takes two parameters: account to check, and the machine name of the permission to check for
+        return AccessResult::allowedIfHasPermission($account, 'view rsvplist');
+      }
     }
 
     return AccessResult::forbidden();
-
-
-    // Some pages may not be nodes, although we could not display the block using the Block Settings through
-    // the Block UI at /admin/structure/block, instead we are programmatically controlling to only display
-    // this block on node pages using AccessResult::allowedIfHasPermission($account, 'view rsvplist')
-
-    // if (!(is_null($node))) {
-    //   $enabler = \Drupal::service('rsvplist.enabler');
-    //   if ($enabler->isEnabled($node)) {
-
-    //     return AccessResult::allowedIfHasPermission($account, 'view rsvplist');
-    //   }
-    // }
-
-    // return AccessResult::forbidden();
   }
 }
